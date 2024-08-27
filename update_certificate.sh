5b7fd2c43e3fa3721dad15744a4687a122309489
ljy2855 <ljy2855@naver.com>
2024-08-28 00:31:43 +0900

    Migrate to ingress node

--- update_certificate.sh
+++ update_certificate.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+# 현재 시각 출력
+echo "Script started at: $(date '+%Y-%m-%d %H:%M:%S')"
+
+# 인자 체크
+if [ -z "$1" ]; then
+  echo "Usage: $0 <workdir>"
+  exit 1
+fi
+
+# Set workdir
+workdir="$1"
+cd "$workdir" || { echo "Failed to change directory to $workdir"; exit 1; }
+
 # 웹서버 OFF
-apache2ctl stop
+echo "Stopping Apache2 service..."
+service apache2 stop
 
 # Certbot 갱신
-certbot renew --non-interactive --manual-auth-hook '/root/dns/godaddy-dns-update.py' --manual-cleanup-hook 'rm -f /tmp/CERTBOT_VALIDATION'
+echo "Renewing certificates..."
+certbot renew --non-interactive --quiet --manual-auth-hook "$workdir/godaddy-dns-update.py" --manual-cleanup-hook 'rm -f /tmp/CERTBOT_VALIDATION'
 
 # 웹서버 ON
-apache2ctl start
+echo "Starting Apache2 service..."
+service apache2 start
+
+# 종료 시각 출력
+echo "Script ended at: $(date '+%Y-%m-%d %H:%M:%S')"
+
