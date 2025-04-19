37360505aa620f50eaf624247cb576c5039234f0
ljy2855 <ljy2855@naver.com>
2025-04-19 10:17:52 +0000

    feat: migrate apache to standalone

--- update_certificate.sh
+++ update_certificate.sh
@@ -3,27 +3,21 @@
 # 현재 시각 출력
 echo "Script started at: $(date '+%Y-%m-%d %H:%M:%S')"
 
-# 인자 체크
-if [ -z "$1" ]; then
-  echo "Usage: $0 <workdir>"
-  exit 1
-fi
-
-# Set workdir
-workdir="$1"
-cd "$workdir" || { echo "Failed to change directory to $workdir"; exit 1; }
+# 현재 디렉토리를 workdir로 설정
+workdir="$PWD"
+echo "Working directory: $workdir"
 
 # 웹서버 OFF
-echo "Stopping Apache2 service..."
-service apache2 stop
+echo "Stopping Nginx service..."
+service nginx stop
 
 # Certbot 갱신
 echo "Renewing certificates..."
 certbot renew --non-interactive --quiet --manual-auth-hook "$workdir/godaddy-dns-update.py" --manual-cleanup-hook 'rm -f /tmp/CERTBOT_VALIDATION'
 
 # 웹서버 ON
-echo "Starting Apache2 service..."
-service apache2 start
+echo "Starting Nginx service..."
+service nginx start
 
 # 종료 시각 출력
 echo "Script ended at: $(date '+%Y-%m-%d %H:%M:%S')"
